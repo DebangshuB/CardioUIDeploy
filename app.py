b4f8dbec10585900bc08b2e7f96598a2aa9dddf7
DebangshuB <debangshu133@gmail.com>
2021-03-16 22:43:14 +0530

    Dark Theme done

--- app.py
+++ app.py
@@ -11,31 +11,25 @@ app.config['SECRET_KEY'] = '18159e1647ba2975'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = Strokedata()
-    theme = 'light'   
-    btnstate = ''
+    form = Strokedata()  
     
     if form.validate_on_submit():
         user_data = model.parse(form)
         session['data'] = user_data
-        session['theme'] = theme
-        session['btnstate'] = 'checked' if theme == 'dark' else '' 
         return redirect(url_for('result'))
 
-    return render_template('home.html', form=form, theme=theme, button_state=btnstate)
+    return render_template('home.html', form=form)
 
 
 @app.route('/results')
 def result():
     prediction = model.GBModel(session['data'])[0]
-    theme = session['theme']
-    return render_template('result.html', res=prediction, title='Results', theme=theme)
+    return render_template('result.html', res=prediction, title='Results')
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    theme = session['theme']
-    return render_template('404.html', theme=theme), 404
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
