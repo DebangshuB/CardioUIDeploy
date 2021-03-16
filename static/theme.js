f6c9d5c83cda54dd939511e219da23d3527a51a8
DebangshuB <debangshu133@gmail.com>
2021-03-16 11:32:29 +0530

    More Dark mode

--- static/theme.js
+++ static/theme.js
@@ -1,15 +1,15 @@
 var checkbox = document.getElementById('flexSwitchCheckDefault');
+var state = 0;
 
 checkbox.addEventListener('change', () => {
     if (checkbox.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        state = 1;
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        state = 0;
     }
 });
-
-
-
 
 
 
