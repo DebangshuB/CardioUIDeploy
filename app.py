3e47e9792ec6235504c66e7372c653299ad45a18
DebangshuB <debangshu133@gmail.com>
2021-03-17 00:08:34 +0530

    Second to final

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def result():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('404.html',title="Error !"), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
